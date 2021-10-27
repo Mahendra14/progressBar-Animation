0b69a5be635632e5723bb944aff451ba4fa5ac33
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-27 15:56:58 +0530

    caluculated % width in the js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const progressBar = document.querySelector("#progress-bar");
 const section = document.querySelector("section")
 
 const animateProgressBar = () => {
+    const scrollDistance = -section.getBoundingClientRect().top;
+    const scrollHeight = section.getBoundingClientRect().height;
+    // subtracting with the height of the html element 
+    //now it ranges from -7 to some 104
+    const progressWidth = (scrollDistance / (scrollHeight - document.documentElement.clientHeight) ) * 100;
+    console.log(progressWidth);
 
 }
 
-animateProgressBar();
+window.addEventListener("scroll",animateProgressBar);
